dae97e9c4dcf6718cffa18fe540e2e895d9b4227
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-09-06 13:41:20 +0200

    router

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const multer = require('multer');
+
+const upload = multer();
 
 const router = express.Router();
 
@@ -8,6 +11,11 @@ router.get('/', function(req, res) {
 
 router.get('/new-user', function(req, res) {
   res.render('new-user');
+});
+
+router.post('/profiles', upload.single('image'), function(req,res) { /*middleware upload une seule image ('nom de l'input dans view'), est execute avant la fonction*/
+const uploadedImageFile = req.file; /*donne acces et store automatiquement le file*/
+const userData = req.body;
 });
 
 module.exports = router;
