f6b927145b290b23650c0e2a058632a8a626a87b
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-09-07 16:58:00 +0200

    finish

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const multer = require('multer');
 
-const upload = multer();
+const db = require('../data/database');
+
+const storageConfig = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'images'); /* cb: callback, null: en cas d erreur, images: path*/
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); /* date.now: temps en milliseconde depuis 1970*/
+  }
+});
+
+const upload = multer({storage: storageConfig}); /* où et comment est stocker l image*/
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get('/',  async function(req, res) {
+  const users = await db.getDb().collection('users').find().toArray();
+  res.render('profiles', {users: users});
 });
 
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
 
-router.post('/profiles', upload.single('image'), function(req,res) { /*middleware upload une seule image ('nom de l'input dans view'), est execute avant la fonction*/
-const uploadedImageFile = req.file; /*donne acces et store automatiquement le file*/
-const userData = req.body;
+router.post('/profiles', upload.single('image'), async function(req,res) { /*middleware upload une seule image ('nom de l'input dans view'), est execute avant la fonction*/
+const uploadedImageFile = req.file; /*pointe le file*/
+const userData = req.body; /*pointe les données rattachées au file*/
+
+await db.getDb().collection('users').insertOne({
+  name: userData.username,
+  imagePath: uploadedImageFile.path
+});
+
+res.redirect('/');
 });
 
 module.exports = router;
