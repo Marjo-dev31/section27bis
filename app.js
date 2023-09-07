ff0706167261b991869d38f43eb6932930ca7664
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-09-07 17:02:02 +0200

    term

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-app.use('/images', express.static('images'));
+app.use('/images', express.static('images')); /*pour etre visible de tous les visiteurs*/
 
 app.use(userRoutes);
 
